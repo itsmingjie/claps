c179ee9629d1a02bf2194cddc44877793b2f0268
Real Awesomeness <RealAwesomeness@outlook.com>
2020-02-01 14:29:11 -0500

    probably mingjie bad

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # thanks literally everyone for not making nodejs style module support
 DATABASE_NAME = "snaps"
-client = MongoClient(os.getenv["MONGODB_URI"])
+client = MongoClient(os.environ["MONGODB_URI"])
 db = client[DATABASE_NAME]
 snaps_collection = db.snaps
 def __init__():
