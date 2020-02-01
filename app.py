5e6ad8a5c6f88c9d363417cd4a42d8aee7728178
Real Awesomeness <RealAwesomeness@outlook.com>
2020-02-01 16:13:55 -0500

    hope for the best

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from dotenv import load_dotenv
 import urllib.parse
 from pymongo import MongoClient
 import os
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 load_dotenv()
 
 # thanks literally everyone for not making nodejs style module support
@@ -13,11 +15,11 @@ db = client[DATABASE_NAME]
 snaps_collection = db.snaps
 def __init__():
     try:
-        client = MongoClient(process.env.MONGODB_URI)
+        client = MongoClient(os.environ["MONGODB_URI"])
         db = client[DATABASE_NAME]
         snaps_collection = db.snaps
     except:
-        print("PLEASE SET UP A DATABSE THX")
+        print("PLEASE SET UP A DATABSE")
         quit()
 
 # Redirect everything to the github page because we don't have a home page
@@ -27,6 +29,7 @@ def index():
 
 # the good stuff
 @app.route('/snap', methods=['POST'])
+@cross_origin()
 def snap():
     url = urllib.parse.quote(urllib.parse.urlparse(request.form['url']).path).replace("/", "")
     num_snaps = int(request.form['num_snaps'])
@@ -43,10 +46,11 @@ def snap():
         new_num_snaps = num_snaps
         snaps_collection.insert_one({"url":url, "num_snaps":new_num_snaps})
     print(snaps_collection.find_one({"url":url}))
-    return url
+    return new_num_snaps
 
 # api to get information about an article
 @app.route('/get_snaps', methods=['GET'])
+@cross_origin()
 def get_snaps():
     url = urllib.parse.quote(urllib.parse.urlparse(request.form['url']).path)
     # find document with matching url and return it
